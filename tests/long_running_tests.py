7cd1c9f7c3f2c049fdba5b870ec424bbe3a5623c
Alexandra Iordache <aghecen@amazon.com>
2020-11-11 16:22:20 +0200

    basic stuff to invoke the api

---
+++ tests/long_running_tests.py
@@ -0,0 +1,16 @@
+PERF_DROP=0 # percentages; modify this to change the test behavior.
+
+THRESHOLD=2 # percentages
+
+if PERF_DROP < 0:
+    print("Performance INCREASED by {}%".format(-PERF_DROP))
+elif PERF_DROP == 0:
+    print("Performance DID NOT CHANGE")
+else:
+    print("Performance DECREASED by {}&".format(PERF_DROP))
+
+# We can only return PASS or FAIL, it's a test after all.
+# The only FAIL condition is decreasing performance by >THRESHOLD.
+# Everything else is PASS.
+if PERF_DROP > THRESHOLD:
+    exit(1)
